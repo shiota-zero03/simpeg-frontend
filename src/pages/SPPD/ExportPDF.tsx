375b74dbd53a5b62db01f00d9ca265a630ef10b7
Shiota Zero <ryukokaru@gmail.com>
2025-04-24 12:30:05 +0700

    update feature terbaru

--- src/pages/SPPD/ExportPDF.tsx
+++ src/pages/SPPD/ExportPDF.tsx
@@ -30,8 +30,17 @@ export default function ExportIKP() {
     }
   }, [id, data]);
 
+  let totalAnggaran = 0;
+
+  DATA_DETAIL?.participants.map(item => {
+    totalAnggaran += Number((item.budgets[0].volDailyAllowance || 0) * (item.budgets[0].dailyAllowance || 0)) + 
+    Number((item.budgets[0].volTransport || 0) * (item.budgets[0].transport || 0)) + 
+    Number((item.budgets[0].volRepresentatif || 0) * (item.budgets[0].representatif || 0));
+  })
+
   useEffect(() => {
     if (!isFetching && DATA_DETAIL) {
+
       // Tunggu render selesai dulu baru trigger print
       setTimeout(() => {
         window.print();
@@ -72,28 +81,28 @@ export default function ExportIKP() {
           <table>
             <tbody>
                 <tr>
-                    <td className="text-[10pt] min-w-72">URAIAN</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.type === "PERJALANAN_BIASA" ? "PERJALANAN DINAS BIASA" : "PERJALANAN DINAS DALAM KOTA"}</td>
+                    <td className="text-[8pt] min-w-72">URAIAN</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.type === "PERJALANAN_BIASA" ? "PERJALANAN DINAS BIASA" : "PERJALANAN DINAS DALAM KOTA"}</td>
                 </tr>
                 <tr>
-                    <td className="text-[10pt] min-w-72">KODE REKENING</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.kodeRekening}</td>
+                    <td className="text-[8pt] min-w-72">KODE REKENING</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.kodeRekening}</td>
                 </tr>
                 <tr>
-                    <td className="text-[10pt] min-w-72">SUB KEGIATAN</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.activity}</td>
+                    <td className="text-[8pt] min-w-72">SUB KEGIATAN</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.activity}</td>
                 </tr>
                 <tr>
-                    <td className="text-[10pt] min-w-72">MAKSUD PERJALANAN</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.reasoning}</td>
+                    <td className="text-[8pt] min-w-72">MAKSUD PERJALANAN</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.reasoning}</td>
                 </tr>
                 <tr>
-                    <td className="text-[10pt] min-w-72">TANGGAL PELAKSANAAN</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.startDate ? DMYIndoToFormat(DATA_DETAIL.startDate) : ""} - {DATA_DETAIL.endDate ? DMYIndoToFormat(DATA_DETAIL.endDate) : ""}</td>
+                    <td className="text-[8pt] min-w-72">TANGGAL PELAKSANAAN</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.startDate ? DMYIndoToFormat(DATA_DETAIL.startDate) : ""} - {DATA_DETAIL.endDate ? DMYIndoToFormat(DATA_DETAIL.endDate) : ""}</td>
                 </tr>
                 <tr>
-                    <td className="text-[10pt] min-w-72">TEMPAT / TUJUAN</td>
-                    <td className="text-[10pt] min-w-72">: {DATA_DETAIL.location}</td>
+                    <td className="text-[8pt] min-w-72">TEMPAT / TUJUAN</td>
+                    <td className="text-[8pt] min-w-72">: {DATA_DETAIL.location}</td>
                 </tr>
             </tbody>
           </table>
@@ -116,7 +125,7 @@ export default function ExportIKP() {
                 <th className="p-1 border border-black bg-blue-100">
                   URAIAN
                 </th>
-                <th className="p-1 border border-black bg-blue-100" colSpan={4}>
+                <th className="p-1 border border-black bg-blue-100" colSpan={5}>
                   BESARAN BIAYA JUMLAH (Rp)
                 </th>
                 <th className="p-1 border border-black bg-blue-100">
@@ -143,6 +152,16 @@ export default function ExportIKP() {
                         <td className="p-1 border border-black align-top text-[10pt] text-right">{item.budgets[0] ? Number(item.budgets[0].dailyAllowance || 0).toLocaleString('id-ID') : 0}</td>
                         <td className="p-1 border border-black align-top text-[10pt] text-center">=</td>
                         <td className="p-1 border border-black align-top text-[10pt] text-right">{item.budgets[0] ? Number((item.budgets[0].volDailyAllowance || 0) * (item.budgets[0].dailyAllowance || 0)).toLocaleString('id-ID') : 0}</td>
+                        <td className="p-1 border border-black text-[10pt] text-right" rowSpan={3}>
+                          {
+                            item.budgets[0] ? 
+                            (
+                              Number((item.budgets[0].volDailyAllowance || 0) * (item.budgets[0].dailyAllowance || 0)) + 
+                              Number((item.budgets[0].volTransport || 0) * (item.budgets[0].transport || 0)) + 
+                              Number((item.budgets[0].volRepresentatif || 0) * (item.budgets[0].representatif || 0))
+                            ).toLocaleString('id-ID') : 0    
+                          }
+                        </td>
                         <td className="p-1 border border-black align-top text-[10pt]" rowSpan={3}>{index + 1}</td>
                     </tr>
                     <tr>
@@ -164,8 +183,51 @@ export default function ExportIKP() {
                 </React.Fragment>
               ))}
             </tbody>
+            <tfoot>
+              <tr>
+                <td className="p-1 border border-black text-[10pt] text-center" colSpan={10}>
+                  JUMLAH SELURUHNYA
+                </td>
+                <td className="p-1 border border-black text-[10pt] text-right" rowSpan={3}>
+                  {
+                    totalAnggaran ? Number(totalAnggaran).toLocaleString('id-ID') : 0 
+                  }
+                </td>
+                <td className="p-1 border border-black text-[10pt] text-center"></td>
+              </tr>
+            </tfoot>
           </table>
           <br />
+          <div className="flex items-end justify-between px-[80pt]">
+            <div className="flex flex-col">
+              <span>Mengetahui / Setuju dibayar</span>
+              <span>Pengguna Anggaran</span>
+              <br />
+              <br />
+              <br />
+              <span className="font-semibold">{DATA_DETAIL.participants.find(it => it.role === "PEGAWAI")?.user.name || "-"}</span>
+              <span>NIP.{DATA_DETAIL.participants.find(it => it.role === "PEGAWAI")?.user.nip || "-"}</span>
+            </div>
+            <div className="flex flex-col">
+              <span>Mengetahui</span>
+              <span>Pejabat Pembuat Komitmen</span>
+              <br />
+              <br />
+              <br />
+              <span className="font-semibold">{DATA_DETAIL.komitmenName || ""}</span>
+              <span>NIP.{DATA_DETAIL.komitmenNip || ""}</span>
+            </div>
+            <div className="flex flex-col">
+              <span>Bekasi,</span>
+              <span>Lunas dibayar,</span>
+              <span>Bendahara Pengeluaran</span>
+              <br />
+              <br />
+              <br />
+              <span className="font-semibold">{DATA_DETAIL.bendaharaName || ""}</span>
+              <span>NIP.{DATA_DETAIL.bendaharaNip || ""}</span>
+            </div>
+          </div>
         </div>
       )}
     </>
