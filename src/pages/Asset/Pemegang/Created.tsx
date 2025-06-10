ab672ebbb5e4c16658e338baebfa0395394973f7
Shiota Zero <ryukokaru@gmail.com>
2025-06-10 16:26:11 +0700

    updat data

--- src/pages/Asset/Pemegang/Created.tsx
+++ src/pages/Asset/Pemegang/Created.tsx
@@ -95,43 +95,53 @@ export default function CreatePegawai() {
     if (role?.includes("UPTD")) {
       if (role === "UPTD_LEGAL") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("METROLOGI LEGAL"),
+          it.jabatan.unit.nameUnit.includes("METROLOGI LEGAL") || 
+          it.jabatan.nameJob.includes("METROLOGI LEGAL"),
         );
       } else if (role === "UPTD_9") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH IX "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH IX ") || 
+          it.jabatan.nameJob.includes("WILAYAH IX "),
         );
       } else if (role === "UPTD_8") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH VIII "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH VIII ") || 
+          it.jabatan.nameJob.includes("WILAYAH VIII "),
         );
       } else if (role === "UPTD_7") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH VII "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH VII ") || 
+          it.jabatan.nameJob.includes("WILAYAH VII "),
         );
       } else if (role === "UPTD_6") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH VI "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH VI ") || 
+          it.jabatan.nameJob.includes("WILAYAH VI "),
         );
       } else if (role === "UPTD_5") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH V "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH V ") || 
+          it.jabatan.nameJob.includes("WILAYAH V "),
         );
       } else if (role === "UPTD_4") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH IV "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH IV ") || 
+          it.jabatan.nameJob.includes("WILAYAH IV "),
         );
       } else if (role === "UPTD_3") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH III "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH III ") || 
+          it.jabatan.nameJob.includes("WILAYAH III "),
         );
       } else if (role === "UPTD_2") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH II "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH II ") || 
+          it.jabatan.nameJob.includes("WILAYAH II "),
         );
       } else if (role === "UPTD_1") {
         return allDataPegawai.data.filter((it) =>
-          it.jabatan.unit.nameUnit.includes("WILAYAH I "),
+          it.jabatan.unit.nameUnit.includes("WILAYAH I ") || 
+          it.jabatan.nameJob.includes("WILAYAH I "),
         );
       } else {
         return allDataPegawai.data;
